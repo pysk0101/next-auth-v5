8ef84a84ab5009156594d39f33c715df1614ab81
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-08-06 19:33:31 +0300

    updateds

--- actions/login.ts
+++ actions/login.ts
@@ -1,15 +1,37 @@
 "use server"
 
 import * as z from "zod";
+import { signIn } from "@/auth"
 import { LoginSchema } from "@/schemas";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { AuthError } from "next-auth";
 
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
-    const validatedFields = LoginSchema.parse(values);
+    const validatedFields = LoginSchema.safeParse(values);
 
-    if (!validatedFields) {
+    if (!validatedFields.success) {
         return { error: "Invalid fields" }
     }
 
-    return { success: "Email was sent!"}
+    const { email, password } = validatedFields.data
+
+    try {
+        await signIn('credentials',{
+            email,
+            password,
+            redirectTo : DEFAULT_LOGIN_REDIRECT
+        })
+    } catch (error) {
+        if(error instanceof AuthError){
+            switch(error.type){
+                case "CredentialsSignin":
+                    return {error : "Invalid Credentials!"}
+                default:
+                    return {error: "Something went wrong"}
+            }
+        }
+        throw error
+    }
+
 }
--- actions/register.ts
+++ actions/register.ts
@@ -1,15 +1,36 @@
 "use server"
 
+import { db } from "@/lib/db";
 import * as z from "zod";
+import bcrypt from "bcryptjs"; 
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.parse(values);
 
+    console.log( "user values",validatedFields)
     if (!validatedFields) {
         return { error: "Invalid fields" }
     }
+    const {email , password, name }= validatedFields
+    const hashedPassword = await bcrypt.hash(password, 10)
 
-    return { success: "Email was sent!"}
+    const existingUser = await getUserByEmail(email)
+    if (existingUser) {
+        return { error: "Email already in use"}
+    }   
+
+    await db.user.create({
+        data : {
+            email,
+            password: hashedPassword,
+            name
+        }
+    })
+
+    //Todo : send email
+
+    return { success: "User Created"}
 }
