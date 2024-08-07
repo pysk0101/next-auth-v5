034795602f3ab730a231966480f849e902bcc350
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-08-07 21:49:15 +0300

    callbacks added

--- auth.ts
+++ auth.ts
@@ -1,3 +1,5 @@
+//Kurulumu baslattigimiz ve  fonksiyonlari dagitima cikardigimiz yer.
+
 import NextAuth from "next-auth"
 import authConfig from "./auth.config"
 import {PrismaAdapter} from '@auth/prisma-adapter'
@@ -6,10 +8,25 @@ import { db } from "./lib/db"
 export const {
     handlers,
     auth,
-    signIn,
+    signIn, 
     signOut
 } =     NextAuth({
-    adapter : PrismaAdapter(db),
+    callbacks : {
+        async session({token,session}){
+
+            session.user.id = token.sub!
+            console.log(session)
+            return session
+        },
+        async jwt({token,user}){
+            console.log({user})
+            console.log({token})
+            return token
+        }
+        
+    },
+    // adapter : PrismaAdapter(db),
     session : {strategy : "jwt"},
+    secret : process.env.NEXTAUTH_SECRET,
     ...authConfig
 })
